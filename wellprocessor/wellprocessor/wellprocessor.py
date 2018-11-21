3a7d821c5cae54fa845a8089fc736f42b9e56184
Rodi Scheele <r.d.scheele@betabit.nl>
2018-11-21 16:40:02 +0100

    changes to architecture test 1

--- wellprocessor/wellprocessor/wellprocessor.py
+++ wellprocessor/wellprocessor/wellprocessor.py
@@ -1,3 +1,4 @@
+import os
 from azure.servicebus import ServiceBusService, Message, Queue
 from azure.storage.blob import BlockBlobService
 import time
@@ -6,7 +7,7 @@ import random
 import ctypes
 
 # Connect to service bus
-bus_service = ServiceBusService(
+'''bus_service = ServiceBusService(
     service_namespace='wellprototype',
     shared_access_key_name='master',
     shared_access_key_value='2uWqgYUl+0PZerFo4qrVPLj1pOiaZGUHDDnXc8I8Umg=')
@@ -26,36 +27,39 @@ if a != 0:
     container_name = msg_list[0]
     blob_item = msg_list[1]
 
-    print('Message list: ' + str(msg_list))
+    print('Message list: ' + str(msg_list))'''
 
-    # Connect to blob service
-    block_blob_service = BlockBlobService(account_name='bbwelldata',
-                                          account_key='jH9MnJI2M7QMZAgAmmYybD7fUYKIzWg45q6lmZqEO4QuhtP1qy3ocsB8YuBuAdEFwgbJFpE1XIGn/ywmSWlrkg==')
+container_name = os.environ['CONTAINER_NAME']
+blob_item = os.environ['BLOB_ITEM']
 
-    # Create a list of blobs within the containers
-    blob_list = block_blob_service.list_blobs(container_name)
-    for item in blob_list:
-        blob_name = item.name
-        if blob_name == blob_item:
-            download_path = './downloads/' + blob_name
-            # block_blob_service.get_blob_to_path(container_name, blob_name, download_path)
-            # print('Downloaded BLOB to ./downloads/ with name: ' + blob_name)
+# Connect to blob service
+block_blob_service = BlockBlobService(account_name='bbwelldata',
+                                      account_key='jH9MnJI2M7QMZAgAmmYybD7fUYKIzWg45q6lmZqEO4QuhtP1qy3ocsB8YuBuAdEFwgbJFpE1XIGn/ywmSWlrkg==')
 
-            time_start = time.time()
-            random_number = random.randint(1, 20)
-            for i in range(1, random_number):
-                a = bytearray(812000000)
-                getcontext().prec = 100
-                random_number = random.randint(100000, 800000)
-                pi = sum(1 / Decimal(16) ** k *
-                         (Decimal(4) / (8 * k + 1) -
-                          Decimal(2) / (8 * k + 4) -
-                          Decimal(1) / (8 * k + 5) -
-                          Decimal(1) / (8 * k + 6)) for k in range(random_number))
-            time_end = time.time()
-            time_diff = time_end - time_start
-            print('Time required to calculate was ' + str(time_diff) + ' seconds')
+# Create a list of blobs within the containers
+blob_list = block_blob_service.list_blobs(container_name)
+for item in blob_list:
+    blob_name = item.name
+    if blob_name == blob_item:
+        download_path = './downloads/' + blob_name
+        # block_blob_service.get_blob_to_path(container_name, blob_name, download_path)
+        # print('Downloaded BLOB to ./downloads/ with name: ' + blob_name)
 
-else:
+        time_start = time.time()
+        random_number = random.randint(1, 20)
+        for i in range(1, random_number):
+            a = bytearray(812000000)
+            getcontext().prec = 100
+            random_number = random.randint(100000, 800000)
+            pi = sum(1 / Decimal(16) ** k *
+                     (Decimal(4) / (8 * k + 1) -
+                      Decimal(2) / (8 * k + 4) -
+                      Decimal(1) / (8 * k + 5) -
+                      Decimal(1) / (8 * k + 6)) for k in range(random_number))
+        time_end = time.time()
+        time_diff = time_end - time_start
+        print('Time required to calculate was ' + str(time_diff) + ' seconds')
+
+'''else:
     print('No items in queue')
-    time.sleep(20)
+    time.sleep(20)'''
--- wellqueue/podqueue/podqueue.py
+++ wellqueue/podqueue/podqueue.py
@@ -14,9 +14,36 @@ bus_service = ServiceBusService(
     shared_access_key_value='2uWqgYUl+0PZerFo4qrVPLj1pOiaZGUHDDnXc8I8Umg=')
 
 
+def deconstruct_message():
+    # Get the top most message
+    msg = bus_service.receive_queue_message('wellqueue', peek_lock=False)
+
+    # Reconstruct the message to be a list
+    msg_body = str(msg.body)
+    msg_body = msg_body[2:]
+    msg_body = msg_body[:-1]
+    msg_list = msg_body.split(';')
+    container_name = msg_list[0]
+    blob_item = msg_list[1]
+    return container_name, blob_item
+
+
 def make_container():
     container = client.V1Container(name="worker")
     container.image = "rdscheele/wellprocessor"
+    # Declare required and max resources
+    container.resources = client.V1ResourceRequirements()
+    container.resources.requests = {'cpu': '0.5', 'memory': '1000Mi'}
+    container.resources.limits = {'cpu': '0.7', 'memory': '2000Mi'}
+    # Declare environment variables with well id
+    container_name, blob_item = deconstruct_message()
+    env_var_container_name = client.V1EnvVar()
+    env_var_container_name.name = 'CONTAINER_NAME'
+    env_var_container_name.value = container_name
+    env_var_blob_item = client.V1EnvVar()
+    env_var_blob_item.name = 'BLOB_ITEM'
+    env_var_blob_item.value = blob_item
+    container.env = [env_var_container_name, env_var_blob_item]
     return container
 
 
@@ -29,19 +56,15 @@ def make_pod():
     container = make_container()
     pod.spec = client.V1PodSpec(containers=[container])
     pod.spec.restart_policy = "Never"
+    pod.spec.termination_grace_period_seconds = 30
 
     return pod
 
 
 def update_queue(core):
-    message_count = bus_service.get_queue('wellqueue').message_count
-
-    if message_count != 0:
-        for i in range(0, message_count):
-            pod = make_pod()
-            core.create_namespaced_pod(namespace, pod)
-            print('Created pod')
-            time.sleep(60)
+    pod = make_pod()
+    core.create_namespaced_pod(namespace, pod)
+    print('Created pod for message ' + str(bus_service.receive_queue_message('wellqueue', peek_lock=True).body))
 
 
 if os.getenv('KUBERNETES_SERVICE_HOST'):
@@ -50,9 +73,20 @@ else:
     config.load_kube_config()
 
 core = client.CoreV1Api()
+pod_list = core.list_namespaced_pod(namespace='default')
+#print(pod_list)
 
-config_map = core.list_namespaced_config_map(namespace='default')
-print(config_map)
-#while True:
-#update_queue(core)
-#time.sleep(10)
+while True:
+    message_count = bus_service.get_queue('wellqueue').message_count
+
+    if message_count != 0:
+        update_queue(core)
+
+    pod_list = core.list_namespaced_pod(namespace='default')
+    for item in pod_list.items:
+        if item.status.phase == 'Succeeded':
+            delete_options = client.V1DeleteOptions()
+            core.delete_namespaced_pod(name=item.metadata.name, namespace='default', body=delete_options)
+            print('Removed terminated pod!')
+
+    time.sleep(10)
