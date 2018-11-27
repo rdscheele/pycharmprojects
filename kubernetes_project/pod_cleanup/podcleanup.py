985fe18201d35ad372f1dafe5d945acce986769a
Rodi Scheele <r.d.scheele@betabit.nl>
2018-11-27 11:06:31 +0100

    cleaned up well processor and added comments

--- kubernetes_project/pod_cleanup/podcleanup.py
+++ kubernetes_project/pod_cleanup/podcleanup.py
@@ -4,31 +4,47 @@ import pytz
 from datetime import datetime
 from kubernetes import client, config
 
+# Get environment variables. These should be defined in the YAML when creating the deployment.
 if os.getenv('KUBERNETES_SERVICE_HOST'):
     config.load_incluster_config()
+    # Timezone is based on where the cluster is hosted, will be UTC for clusters hosted in EUW
     tz = os.environ['TIME_ZONE']
     timezone = pytz.timezone(tz)
-    max_time_diff = int(os.environ['TIME_DIFF_BEFORE_DELETION'])
+    # How long ago a pod has to be created before being deleted in seconds
+    max_time_diff = int(os.environ['TIME_EXPIRED_BEFORE_DELETION'])
+    # Set the namespace where the pods are located
+    namespace = os.environ['POD_NAMESPACE']
+    # How long the cleanup waits before checking for pods and executing again
+    sleep_time = os.environ['SLEEP_TIME']
 else:
     config.load_kube_config()
     timezone = pytz.utc
     max_time_diff = 1800
+    namespace = "default"
+    sleep_time = 60
 
+# Load core kubernetes API
 core = client.CoreV1Api()
 
 while True:
-    pod_list = core.list_namespaced_pod(namespace='default')
-    if len(pod_list.items) != 0:
-        for item in pod_list.items:
+    # Get the current list of pods in the default namespace
+    pod_list = core.list_namespaced_pod(namespace='default').items
+    if len(pod_list) != 0:
+        for item in pod_list:
+            # Get the phase of the pod
             item_phase = item.status.phase
+            # Get the start time of the pod
             item_datetime_start = item.status.start_time
             now_datetime = datetime.now(timezone)
             diff_datetime = int((now_datetime - item_datetime_start).seconds)
+            # If a pod has status succeeded and has lived longer than specified time
+            # A pod gets status.phase succeeded when it has terminated successfully
+            # More on phase https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase
             if item_phase == 'Succeeded' and diff_datetime > max_time_diff:
                 delete_options = client.V1DeleteOptions()
-                core.delete_namespaced_pod(name=item.metadata.name, namespace='default', body=delete_options)
+                core.delete_namespaced_pod(name=item.metadata.name, namespace=namespace, body=delete_options)
                 pod_name = str(item.metadata.name)
                 print('Removed terminated pod ' + pod_name)
     else:
         print('No pods to be deleted.')
-    time.sleep(60)
+    time.sleep(sleep_time)
--- kubernetes_project/well_processor/wellprocessor.py
+++ kubernetes_project/well_processor/wellprocessor.py
@@ -4,22 +4,30 @@ import time
 from decimal import Decimal, getcontext
 import psutil
 
+# Get environment variables. These will automatically be set in the YAML by the queue manager.
 if os.getenv('KUBERNETES_SERVICE_HOST'):
+    # Name of the BLOB container in storage
     container_name = os.environ['CONTAINER_NAME']
+    # Name of the item (picture) inside the container
     blob_item = os.environ['BLOB_ITEM']
+    # Storage account credentials
     storage_account_name = os.environ['STORAGE_ACCOUNT_NAME']
     storage_account_key = os.environ['STORAGE_ACCOUNT_KEY']
+    # Memory to be used by this container. Integer where each number is one byte, will be byte array.
+    # Should never be set higher than 800.000.000 or 800MB
     memory_usage = int(os.environ['FAKE_MEMORY_USAGE'])
+    # CPU to be used by this container. Integer where each number is one calculation of pi.
     cpu_usage = int(os.environ['FAKE_CPU_USAGE'])
+    # ID (name) of the current pod
     pod_id = os.environ['POD_ID']
 else:
     container_name = 'test'
     with open('C:/Users/r.d.scheele/OneDrive - Betabit/Keys/storage_account_key.txt', 'r') as myfile:
         storage_account_key = myfile.read()
     storage_account_name = 'bbwelldata'
-    blob_item = 'vlag_met_geit.jpg'
+    blob_item = 'Vlag met geit 9.jpg'
     memory_usage = 400000000
-    cpu_usage = 10
+    cpu_usage = 5
     pod_id = 'INVALID'
 
 print('This pod has ID ' + pod_id)
@@ -27,28 +35,32 @@ print('This pod has ID ' + pod_id)
 # Connect to blob service
 block_blob_service = BlockBlobService(account_name=storage_account_name, account_key=storage_account_key)
 
-# Create a list of blobs within the containers
+# Find the blob container that this pod has to analyse
 blob_list = block_blob_service.list_blobs(container_name)
+# Find the item (picture) in the blob container
+# There are .sleep() functions in this code to make the analysis take a bit longer.
 for item in blob_list:
     blob_name = item.name
     if blob_name == blob_item:
-        # download_path = './downloads/' + blob_name
-        # block_blob_service.get_blob_to_path(container_name, blob_name, download_path)
-        # print('Downloaded BLOB to ./downloads/ with name: ' + blob_name)
+        time.sleep(2)
+        # Delete the item from the container
+        block_blob_service.delete_blob(container_name=container_name, blob_name=blob_name)
         time.sleep(2)
         print(psutil.virtual_memory())
+        time.sleep(2)
         time_start = time.time()
         pi = 0
-        for i in range(1, cpu_usage):
+        for i in range(0, cpu_usage):
+            # Create byte array which can consume RAM
             a = bytearray(memory_usage)
             getcontext().prec = 100
+            # Calculate PI
             pi = sum(1 / Decimal(16) ** k *
                      (Decimal(4) / (8 * k + 1) -
                       Decimal(2) / (8 * k + 4) -
                       Decimal(1) / (8 * k + 5) -
                       Decimal(1) / (8 * k + 6)) for k in range(500000))
             time.sleep(2)
-            print(psutil.virtual_memory())
         print('Pi is calculated to be ' + str(pi) + ' ')
         time_end = time.time()
         time_diff = time_end - time_start
@@ -57,3 +69,4 @@ for item in blob_list:
         print('Time required to calculate was ' + str(time_diff) + ' seconds for BLOB container item ' + str(blob_item))
         time.sleep(2)
         print(psutil.virtual_memory())
+        # Done. Pod should terminate successfully here.
